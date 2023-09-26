5200aff0281a2f2b2e15edde07bd48d442939e23
JMER_Delly <juanesscorcia@gmail.com>
2023-09-26 15:47:31 -0600

    _max_loops allows large n

--- Applications/HumanMouse/mouse_analysis.py
+++ Applications/HumanMouse/mouse_analysis.py
@@ -27,86 +27,87 @@ def extract_networks(directory_name=str, files_ignore=list, cols=tuple, feo=True
           networks[name] = net
     return networks
 
-# Biological networks
-mm9CellTypeValues = { "m3134-DS8497" : 			  "mammary adenocarcinoma",
-                        "m416B-DS14099" : 		  "CD34+ myeloid progenitor",
-                        "mA20-DS16695" :            "B-lymphocyte reticulum cell sarcoma",
-                        "mATn-DS17070" :            "activated T-naive cell",
-                        "mATr-DS20149" :            "ATr",
-                        "mB-DS16168" :              "B-cell, replicate 1",
-                        "mB-DS17866" :              "B-cell, replicate 2",
-                        "mBrain-DS12727" :          "normal brain",
-                        "mCJ7-DS13320" :            "embryonic stem cells via 129S1/SVImJ mice",
-                        "MEL-DS13036" :             "leukemia, K562 analog",
-                        "mfBrain-DS14536" :         "fetal brain",
-                        "mGenitalFatPad-DS18182" :  "genital adipose tissue",
-                        "mHeart-DS18138" :          "normal heart",
-                        "mKidney-DS13948" :         "normal kidney",
-                        "mLiver-DS14605" :          "normal liver",
-                        "mLung-DS14479" :           "normal lung",
-                        "mNIH_3T3-DS16900" :        "fibroblast",
-                        "mRetina-DS20004" :         "normal retina", 
-                        "mThymus-DS18819" :         "normal thymus",
-                        "mTN-DS16171" :             "TN, replicate 1",
-                        "mTN-DS17080" :             "TN, replicate 2",
-                        "mTR-DS17864" :             "regulatory T-cells",
-                        "ZhBTc4-DS15236" :          "embryonic stem cells, +6 hrs doxycycline",
-                        "ZhBTc4-DS17562" :          "embryonic stem cells, +24 hrs doxycycline",
-                        "ZhBTc4-DS17616" :          "embryonic stem cells, control"
-                        }
+if __name__ == '__main__':
+    # Biological networks
+    mm9CellTypeValues = { "m3134-DS8497" : 			  "mammary adenocarcinoma",
+                            "m416B-DS14099" : 		  "CD34+ myeloid progenitor",
+                            "mA20-DS16695" :            "B-lymphocyte reticulum cell sarcoma",
+                            "mATn-DS17070" :            "activated T-naive cell",
+                            "mATr-DS20149" :            "ATr",
+                            "mB-DS16168" :              "B-cell, replicate 1",
+                            "mB-DS17866" :              "B-cell, replicate 2",
+                            "mBrain-DS12727" :          "normal brain",
+                            "mCJ7-DS13320" :            "embryonic stem cells via 129S1/SVImJ mice",
+                            "MEL-DS13036" :             "leukemia, K562 analog",
+                            "mfBrain-DS14536" :         "fetal brain",
+                            "mGenitalFatPad-DS18182" :  "genital adipose tissue",
+                            "mHeart-DS18138" :          "normal heart",
+                            "mKidney-DS13948" :         "normal kidney",
+                            "mLiver-DS14605" :          "normal liver",
+                            "mLung-DS14479" :           "normal lung",
+                            "mNIH_3T3-DS16900" :        "fibroblast",
+                            "mRetina-DS20004" :         "normal retina", 
+                            "mThymus-DS18819" :         "normal thymus",
+                            "mTN-DS16171" :             "TN, replicate 1",
+                            "mTN-DS17080" :             "TN, replicate 2",
+                            "mTR-DS17864" :             "regulatory T-cells",
+                            "ZhBTc4-DS15236" :          "embryonic stem cells, +6 hrs doxycycline",
+                            "ZhBTc4-DS17562" :          "embryonic stem cells, +24 hrs doxycycline",
+                            "ZhBTc4-DS17616" :          "embryonic stem cells, control"
+                            }
 
-mouse_networks = extract_networks('HumanMouseData\mouse_net', [], (3,4), feo=True, code_names=mm9CellTypeValues)
+    mouse_networks = extract_networks('HumanMouseData\mouse_net', [], (3,4), feo=True, code_names=mm9CellTypeValues)
 
-# Synthetic networks
-from freyrelab.nets import models
+    # Synthetic networks
+    from freyrelab.nets import models
 
-seed = 42
-synth_graphs = {}
+    seed = 42
+    synth_graphs = {}
 
-for net_id, G in mouse_networks.items():
-    n = G.number_of_nodes()
-    m = G.number_of_edges()
-    for i in range(1,11):
-        synth_graphs[f'BA_{net_id}_{i}'] = models.barabasi_albert_graph(n)
-        synth_graphs[f'ER_{net_id}_{i}'] = nx.erdos_renyi_graph(n, m/(n*(n-1)), seed=seed, directed=False)
+    for net_id, G in mouse_networks.items():
+        n = G.number_of_nodes()
+        m = G.number_of_edges()
+        for i in range(1,11):
+            synth_graphs[f'BA_{net_id}_{i}'] = models.barabasi_albert_graph(n)
+            synth_graphs[f'ER_{net_id}_{i}'] = nx.erdos_renyi_graph(n, m/(n*(n-1)), seed=seed, directed=False)
 
-networks = {**mouse_networks, **synth_graphs}
+    networks = {**mouse_networks, **synth_graphs}
 
-# Graph info
-nets_info = {'Cell Type': [],
-        'Nodes': [],
-        'Edges': [],
-        'Density': []
-        }
+    # Graph info
+    nets_info = {'Cell Type': [],
+            'Nodes': [],
+            'Edges': [],
+            'Density': []
+            }
 
-for name,G in networks.items():
-    n_nodes = G.number_of_nodes()
-    n_edges = G.number_of_edges()
-    density = n_edges / (n_nodes * (n_nodes - 1))
-    nets_info['Cell Type'].append(name)
-    nets_info['Nodes'].append(n_nodes)
-    nets_info['Edges'].append(n_edges)
-    nets_info['Density'].append(density)
+    for name,G in networks.items():
+        n_nodes = G.number_of_nodes()
+        n_edges = G.number_of_edges()
+        density = n_edges / (n_nodes * (n_nodes - 1))
+        nets_info['Cell Type'].append(name)
+        nets_info['Nodes'].append(n_nodes)
+        nets_info['Edges'].append(n_edges)
+        nets_info['Density'].append(density)
 
-info_df = pd.DataFrame.from_dict(nets_info)
-path = os.getcwd()
-path += '\mouse_analysis_nets_info'
-pd.DataFrame.to_csv(info_df, path_or_buf= path, mode='w')
+    info_df = pd.DataFrame.from_dict(nets_info)
+    path = os.getcwd()
+    path += '\mouse_analysis_nets_info'
+    pd.DataFrame.to_csv(info_df, path_or_buf= path, mode='w')
 
-# Netective Analysis
-import pickle
-from netective import compare_structure
+    # Netective Analysis
+    import pickle
+    from netective import compare_structure
 
 
-name_scalars_array, name_moments_array = compare_structure(networks, norm='biological', workers=6, return_prop_dicts=True)
-# name_scalars_array, name_moments_array = compare_structure(networks, norm='biological', workers='auto', return_prop_dicts=True)
+    name_scalars_array, name_moments_array = compare_structure(networks, norm='biological', workers=6, return_prop_dicts=True)
+    # name_scalars_array, name_moments_array = compare_structure(networks, norm='biological', workers='auto', return_prop_dicts=True)
 
-# Save results
-with open('mouse_analysis_scalars.pkl', 'wb') as f:
-    pickle.dump(name_scalars_array, f)
+    # Save results
+    with open('mouse_analysis_scalars.pkl', 'wb') as f:
+        pickle.dump(name_scalars_array, f)
 
-with open('mouse_analysis_moments.pkl', 'wb') as f:
-    pickle.dump(name_moments_array, f)
+    with open('mouse_analysis_moments.pkl', 'wb') as f:
+        pickle.dump(name_moments_array, f)
 
 
 
